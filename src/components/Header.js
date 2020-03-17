3051d0886d3b6daf7ce3f6b361e7872e576825db
vanesa-tamu <vahe5495@tamu.edu>
2020-03-17 17:00:28 -0500

    added name prop to header and deleted props from original state hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,13 @@ const Header = props => {
       <figure className="image is-128x128">
         <img src={props.image} alt={props.name} />
       </figure>
-      <h2>{props.car.name}</h2>
+      <h2>{props.name}</h2>
       <p>Amount: ${props.price}</p>
     </>
   );
 };
 
-const mapStatetoProps = (state) => {
+const mapStateToProps = (state) => {
   const { image, name, price} = state.car
   return {
     image,
@@ -23,4 +23,4 @@ const mapStatetoProps = (state) => {
   }
 }
 
-export default connect(mapStatetoProps)(Header);
+export default connect(mapStateToProps)(Header);
